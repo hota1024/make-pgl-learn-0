959835b64e2ae85f53757c2d7996e3cc0b8f0309
hota1024 <hotalog@hota1024.com>
2020-05-18 18:06:11 +0900

    🏷️ Add src/interfaces/Lexer.ts

---
+++ src/interfaces/Lexer.ts
@@ -0,0 +1,13 @@
+import { TokenInterface } from '../Token'
+
+/*
+ * Lexer interface.
+ */
+export interface LexerInterface {
+  /**
+   * Analyze source code and returns tokens.
+   *
+   * @param source Source code.
+   */
+  analyze(source: string): TokenInterface[]
+}
