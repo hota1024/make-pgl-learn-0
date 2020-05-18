111927138f2fb38cb624bf22e7f45a3a2a8319c5
hota1024 <hotalog@hota1024.com>
2020-05-18 22:29:25 +0900

    🏷️ Update Symbol token

--- src/abstracts/tokens/SymbolToken.ts
+++ src/abstracts/tokens/SymbolToken.ts
@@ -1,23 +1,6 @@
 import { Token } from './Token'
-import { Pos } from '../../classes'
 
 /*
  * Symbol token class.
  */
-export class SymbolToken extends Token {
-  /**
-   * Symbol string.
-   */
-  readonly symbol: string
-
-  /**
-   * SymbolToken constructor.
-   *
-   * @param symbol Symbol string.
-   * @param pos Token position.
-   */
-  constructor(symbol: string, pos: Pos) {
-    super(pos)
-    this.symbol = symbol
-  }
-}
+export class SymbolToken extends Token {}
