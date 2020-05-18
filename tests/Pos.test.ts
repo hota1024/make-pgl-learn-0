2eee9c2872d041d4b9734e001eb846b608aa3adc
hota1024 <hotalog@hota1024.com>
2020-05-18 18:21:57 +0900

    Update tests/Pos.test.ts

--- tests/Pos.test.ts
+++ tests/Pos.test.ts
@@ -12,13 +12,13 @@ describe('Pos class test', () => {
   test('should be merged', () => {
     const a0 = new Pos(3, 19)
     const a1 = new Pos(1, 5)
-    const a2 = a0.marge(a1)
+    const a2 = a0.merge(a1)
     expect(a2.start).toBe(1)
     expect(a2.end).toBe(19)
 
     const b0 = new Pos(1, 20)
     const b1 = new Pos(4, 6)
-    const b2 = b0.marge(b1)
+    const b2 = b0.merge(b1)
     expect(b2.start).toBe(1)
     expect(b2.end).toBe(19)
   })
