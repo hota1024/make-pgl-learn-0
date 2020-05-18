e808c488cd3f1d3b0510778158fb044d3f4a7bd4
hota1024 <hotalog@hota1024.com>
2020-05-18 20:18:49 +0900

    Update tests/Token.test.ts

--- tests/Token.test.ts
+++ tests/Token.test.ts
@@ -1,3 +1,4 @@
+import { Token } from '../src/abstracts'
 import { Pos } from '../src/classes'
 
 describe('Token class test', () => {
@@ -9,12 +10,19 @@ describe('Token class test', () => {
 
     const token1 = new Token('test', new Pos(0, 3))
     expect(token1.value).toBe('test')
-    expect(token0.pos.start).toBe(0)
-    expect(token0.pos.end).toBe(3)
+    expect(token1.pos.start).toBe(0)
+    expect(token1.pos.end).toBe(3)
 
-    const token1 = new Token(10, new Pos(0, 1))
-    expect(token1.value).toBe(19)
-    expect(token0.pos.start).toBe(0)
-    expect(token0.pos.end).toBe(3)
+    const token2 = new Token(10, new Pos(0, 1))
+    expect(token2.value).toBe(10)
+    expect(token2.pos.start).toBe(0)
+    expect(token2.pos.end).toBe(1)
+  })
+
+  test('should throw an error', () => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+    // @ts-ignore
+    expect(() => new Token()).toThrow()
+    // @ts-check
   })
 })
