73057f7c622e18438d8a607d284c4465d3c09bce
hota1024 <hotalog@hota1024.com>
2020-05-19 13:26:46 +0900

    🏷️ Add SpaceRule class

---
+++ src/impls/rules/SpaceRule.ts
@@ -0,0 +1,20 @@
+import { LexerRuleInterface } from '../../interfaces'
+import { LexerAnalyzeContext } from '../../types'
+import { SpaceToken } from '../tokens/SpaceToken'
+import { Pos } from '../../classes'
+
+/*
+ * Space rule class.
+ */
+export class SpaceRule implements LexerRuleInterface {
+  validate({ char }: LexerAnalyzeContext): boolean {
+    return char() === ' '
+  }
+
+  execute({ current, forward }: LexerAnalyzeContext): SpaceToken {
+    const token = new SpaceToken(new Pos(current(), current() + 1))
+    forward()
+
+    return token
+  }
+}
