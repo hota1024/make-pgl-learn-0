594d830e49334c5bebf72fd0a37737c90178d5f3
hota1024 <hotalog@hota1024.com>
2020-05-20 21:57:09 +0900

    Update src/impls/rules/index.ts

--- src/impls/rules/index.ts
+++ src/impls/rules/index.ts
@@ -1,3 +1,14 @@
+import {
+  AlphabetRule,
+  InlineCommentRule,
+  NewLineRule,
+  NumberRule,
+  RangeCommentRule,
+  SpaceRule,
+  StringRule,
+  SymbolsRule,
+} from '..'
+
 export * from './AlphabetRule'
 export * from './InlineCommentRule'
 export * from './NewLineRule'
@@ -6,3 +17,14 @@ export * from './RangeCommentRule'
 export * from './SpaceRule'
 export * from './StringRule'
 export * from './SymbolsRule'
+
+export const AllRules = [
+  new AlphabetRule(),
+  new InlineCommentRule(),
+  new NewLineRule(),
+  new NumberRule(),
+  new RangeCommentRule(),
+  new SpaceRule(),
+  new StringRule(),
+  new SymbolsRule(),
+]
