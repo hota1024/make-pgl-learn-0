fbac5ba151f89531b34e97322e080045f6224a8c
hota1024 <hotalog@hota1024.com>
2020-05-20 17:33:38 +0900

    🏷️ Update InlineCommenttoken class

--- src/impls/tokens/comments/InlineCommentToken.ts
+++ src/impls/tokens/comments/InlineCommentToken.ts
@@ -1,6 +1,24 @@
 import { CommentToken } from '../../../abstracts'
+import { Pos } from '../../../classes'
 
 /*
  * InlineComment token class.
  */
-export class InlineCommentToken extends CommentToken {}
+export class InlineCommentToken extends CommentToken {
+  /**
+   * Inline comment prefix.
+   */
+  readonly prefix: string
+
+  /**
+   * InlineCommentToken constructor.
+   *
+   * @param prefix Comment prefix.
+   * @param content Comment content.
+   * @param pos Token position.
+   */
+  constructor(prefix: string, content: string, pos: Pos) {
+    super(content, pos)
+    this.prefix = prefix
+  }
+}
