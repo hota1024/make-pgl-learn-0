652502613a415edce21cbf37b726cd56d4d7dee2
hota1024 <hotalog@hota1024.com>
2020-05-20 20:40:43 +0900

    🏷️ Update RangeCommentToken calss

--- src/impls/tokens/comments/RangeCommentToken.ts
+++ src/impls/tokens/comments/RangeCommentToken.ts
@@ -1,6 +1,31 @@
 import { CommentToken } from '../../../abstracts'
+import { Pos } from '../../../classes'
 
 /*
  * RangeComment token class.
  */
-export class RangeCommentToken extends CommentToken {}
+export class RangeCommentToken extends CommentToken {
+  /**
+   * Range comment prefix.
+   */
+  readonly prefix: string
+
+  /**
+   * Range comment suffix.
+   */
+  readonly suffix: string
+
+  /**
+   * RangeCommentToken constructor.
+   *
+   * @param prefix Comment prefix.
+   * @param suffix Comment suffix.
+   * @param content Comment content.
+   * @param pos Token position.
+   */
+  constructor(prefix: string, suffix: string, content: string, pos: Pos) {
+    super(content, pos)
+    this.prefix = prefix
+    this.suffix = suffix
+  }
+}
